4efb16cc78e7f7945c55799da7d05030a021f2b9
Real-Musafir <alshahadath@gmail.com>
2022-10-08 15:28:59 +0600

    connect to the echo server

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,10 @@ const ECHO_SERVER_ADDRESS: &str = "localhost:1234";
 fn main() {
     // connection
     println!("Connecting to {}", ECHO_SERVER_ADDRESS);
+
+    if let Ok(mut stream) = TcpStream::connect(ECHO_SERVER_ADDRESS) {
+        println!("connected to echo server {}", ECHO_SERVER_ADDRESS);
+    } else {
+        println!("failed to connect to echo server {}", ECHO_SERVER_ADDRESS);
+    }
 }
