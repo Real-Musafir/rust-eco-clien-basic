ed0e82ab581bdb9f1d5085609aafa8c2cff4158c
Real-Musafir <alshahadath@gmail.com>
2022-10-08 17:04:38 +0600

    send hello world in echo server

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,18 @@ fn main() {
     println!("Connecting to {}", ECHO_SERVER_ADDRESS);
 
     if let Ok(mut stream) = TcpStream::connect(ECHO_SERVER_ADDRESS) {
-        println!("connected to echo server {}", ECHO_SERVER_ADDRESS);
+        // connected
+        println!(
+            "connected to echo server {}:{}",
+            stream.local_addr().unwrap().ip(),
+            stream.local_addr().unwrap().port()
+        );
+
+        //write a hello world message
+        let message: &str = "Hello World";
+        let _ = stream.write(message.as_bytes());
+        let _ = stream.flush();
+        println!("sent: {}", message);
     } else {
         println!("failed to connect to echo server {}", ECHO_SERVER_ADDRESS);
     }
